f760727d074a22208dabf4edd315bcf4bdd76d97
Bokang Ndaba <ndababokang440@gmail.com>
2023-03-31 00:49:42 +0200

    Update ReverseInt.java
    Started by getting int user input and converting it to string. Now we loop through the string and try methods to reverse it

--- ReverseInt.java
+++ ReverseInt.java
@@ -1,3 +1,21 @@
+import java.util.*;
+
 public class ReverseInt {
+    public static void main (String [] args)
+    {
     
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter your number: ");
+        int num = sc.nextInt();
+        String number = Integer.toString(num);
+        for(int i = 0; i<number.length(); i++)
+        {
+            
+        }
+
+
+
+
+
+    }
 }
