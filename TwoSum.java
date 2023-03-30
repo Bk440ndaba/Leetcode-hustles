68e64ca6cac2e57d6c1f5c18385b9bfc8bb0bc98
Bokang Ndaba <ndababokang440@gmail.com>
2023-03-30 23:07:33 +0200

    Update TwoSum.java
    wrote the first part of the code. I used two for loops to loop through the array and compare each and every element. So far not good

---
+++ TwoSum.java
@@ -0,0 +1,33 @@
+import java.util.*;
+
+public class TwoSum {
+    public static void main(String [] args)
+    {
+        int [] nums = {2, 7, 9, 15};
+        int target = 9;
+
+        ArrayList<Integer> number = new ArrayList<>();
+        for(int i = 0; i<nums.length; i++)
+        {
+            for(int k = 0; k<nums.length; k++)
+            {
+                if(nums[i] + nums[k] == target)
+                {
+                    number.add(i);
+                    number.add(k);
+                    for(int m = 0; m<number.size(); m++)
+                    {
+                        System.out.println("Output: " + nums[m]);
+                    }
+
+                }
+
+                else
+                {
+                    System.out.println("No target found.");
+                }
+            }
+        }
+
+    }
+}
