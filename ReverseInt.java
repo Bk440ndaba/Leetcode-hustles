c86bb99a2d58c8d9f4e06754938526ecfefbae26
Bokang Ndaba <ndababokang440@gmail.com>
2023-03-31 02:52:53 +0200

    Update ReverseInt.java
    Dealing with signed integers

--- ReverseInt.java
+++ ReverseInt.java
@@ -12,13 +12,25 @@ public class ReverseInt {
         //int number2 = number.length();
        // System.out.println(number2);
       // char [] arr = new char[number.length()];
-        
+      
+      if(number.contains("-"))
+      {
+        System.out.println("yes");
+      }
+      else
+      {
         for(int i = number.length()-1; i>=0; i--)
         {
             System.out.println(number.charAt(i));
             //arr[i] = number.charAt(i);
            // i++;
         }
+      }
+
+
+      
+        
+       
        // System.out.println();
 
         /*for(int k = 0; k<arr.length; k++)
